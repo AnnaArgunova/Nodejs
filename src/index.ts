6aa1d2267badb0ada84019a3ba94ae64f037c7a9
anna <anna.argunova@gmail.com>
2025-03-06 13:58:07 +0200

    add deps

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, { Express, Request, Response } from "express";
+import {logger} from "@utils/logger";
+
+const app: Express = express();
+const port = process.env.PORT || 5000;
+
+app.get("/", (req: Request, res: Response) => {
+    console.log(logger());
+    res.send("Server");
+});
+
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+});
---
+++ src/utils/logger.ts
@@ -0,0 +1,3 @@
+export const logger = ()=> {
+    return 2
+}
