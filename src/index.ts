957a4d497457683a5d8df5e52bd4ef07bedec08a
anna <anna.argunova@gmail.com>
2025-03-06 15:23:44 +0200

    delete run build

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,11 @@ app.get("/", (req: Request, res: Response) => {
     res.send("Server starting");
 });
 
+app.get("/test", (req: Request, res: Response) => {
+    console.log(logger());
+    res.json({ test: "test" });
+});
+
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
