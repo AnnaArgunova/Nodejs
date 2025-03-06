294fdb08519e744f7af2ada6e9d43ca30dc4e104
anna <anna.argunova@gmail.com>
2025-03-06 18:00:48 +0200

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
 import express, { Express, Request, Response } from "express";
 import {logger} from "@utils/logger";
+import cors from 'cors';
 
 export const app: Express = express();
+
+const corsOptions = {
+    origin: ['http://localhost:3000', 'https://blog-nextjs-365yv544m-anna-argunovas-projects.vercel.app'],
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
 
 app.get("/", (req: Request, res: Response) => {
     console.log(logger());
